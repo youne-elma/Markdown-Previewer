1a7345192b07a577a64ec04166e0cd0bb2d19792
younelma <youne.elma@gmail.com>
2022-10-27 22:08:29 +0100

    feat[MP]: add the marked package

--- src/pages/components/Preview/index.jsx
+++ src/pages/components/Preview/index.jsx
@@ -1,15 +1,25 @@
 import PropTypes from "prop-types";
+import { marked } from "marked";
 import "./preview.scss";
 
 function Preview({ markdown }) {
+  marked.setOptions({
+    breaks: true,
+  });
+  const renderer = new marked.Renderer();
   return (
     <div className="previewerWrap">
       <div className="toolbar">
         <h4>Previewer</h4>
       </div>
-      <div className="preview" id="preview">
-        {markdown}
-      </div>
+      <div
+        className="preview"
+        id="preview"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: marked(markdown, { renderer }),
+        }}
+      />
     </div>
   );
 }
