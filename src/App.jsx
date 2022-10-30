96681f83380e9bf5085016d98cb489c738e43309
younelma <youne.elma@gmail.com>
2022-10-30 20:22:23 +0100

    feat[MP]: add the react router dom, set the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import ErrorPage from "./pages/error/index";
 import Main from "./pages/main/index";
 
 function App() {
-  return <Main />;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="markdown-previewer/"
+          exact
+          element={
+            <div id="wrapper" className="main">
+              <Main />
+            </div>
+          }
+        />
+        <Route path="markdown-previewer/*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
