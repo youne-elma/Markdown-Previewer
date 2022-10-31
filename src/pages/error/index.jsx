1e2510f6feb774f1b79e87793d81aedbcb34f631
younelma <youne.elma@gmail.com>
2022-10-31 20:39:49 +0100

    feat[MP]: Error Page design and structure

---
+++ src/pages/error/index.jsx
@@ -0,0 +1,16 @@
+import "./error.scss";
+import { Link } from "react-router-dom";
+
+function ErrorPage() {
+  return (
+    <div className="error-main">
+      <h1 className="errorTitle">Error 404</h1>
+      <Link to="/markdown-previewer/" className="goBackButton">
+        Go Back
+      </Link>
+      <h4 className="authorErrorPage">Markdown Previewer made by Younelma</h4>
+    </div>
+  );
+}
+
+export default ErrorPage;
